add17a2d3908374c1cfbd31df0314cd0d52976bd
adamsilverstein <adamsilverstein@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-10 03:26:07 +0000

    cleaned up code, especially white space
    git-svn-id: https://plugins.svn.wordpress.org/user-admin-simplifier/trunk@623269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- useradminsimplifier.php
+++ useradminsimplifier.php
@@ -1,9 +1,9 @@
 <?php
  /*
 Plugin Name: User Admin Simplifier
-Plugin URI: http://www.earthbound.com/plugins/user-admin-simplifier.php
+Plugin URI: http://www.earthbound.com/plugins/user-admin-simplifier
 Description: Lets any Administrator simplify the WordPress Admin interface, on a per-user basis, by turning specific menu/submenu sections off.
-Version: 0.4.2
+Version: 0.4.5
 Author: Adam Silverstein
 Author URI: http://www.earthbound.com/plugins
 License: GPLv2 or later
@@ -31,19 +31,19 @@ License: GPLv2 or later
 		$uas_options=uas_get_admin_options();
 		$newmenu=array();
 		//rebuild menu based on saved options
-		foreach ($menu as $menuitem) {
-			if  ( isset ( $menuitem[5] ) && isset( $uas_options[$current_user->user_login][$menuitem[5]] ) &&
-	 	 			1 == $uas_options[$current_user->user_login][$menuitem[5]] ) {
-  				remove_menu_page($menuitem[2]);
+		foreach ( $menu as $menuitem ) {
+			if  ( isset ( $menuitem[5] ) && isset( $uas_options[ $current_user->user_login ][ $menuitem[5] ] ) &&
+	 	 			1 == $uas_options[ $current_user->user_login ][ $menuitem[5] ] ) {
+  				remove_menu_page( $menuitem[2] );
 			} else {
 				// lets check the submenus
-				if ( isset ( $storedsubmenu[$menuitem[2]] ) ) {
-					foreach ($storedsubmenu[$menuitem[2]] as $subsub) {	
+				if ( isset ( $storedsubmenu[ $menuitem[2] ] ) ) {
+					foreach ( $storedsubmenu[ $menuitem[2] ] as $subsub ) {	
 						$combinedname=$menuitem[5].$subsub[2];
-						if  ( isset ( $subsub[2] ) && isset( $uas_options[$current_user->user_login][$menuitem[5].$subsub[2]] ) &&
-							1 == $uas_options[$current_user->user_login][$menuitem[5].$subsub[2]] ) {
- 								remove_submenu_page($menuitem[2],$subsub[2]);
-							}
+						if  ( isset ( $subsub[2] ) && isset( $uas_options[ $current_user->user_login ][ $menuitem[5].$subsub[2] ] ) &&
+							1 == $uas_options[ $current_user->user_login ][ $menuitem[5].$subsub[2] ] ) {
+ 								remove_submenu_page( $menuitem[2],$subsub[2] );
+						}
 					}
 				}
 			}
@@ -52,7 +52,7 @@ License: GPLv2 or later
 	
 	function uas_plugin_action_links( $links, $file ) {
  		if ( $file == plugin_basename( __FILE__ ) ) {
-			$posk_links = '<a href="'.get_admin_url().'admin.php?page=useradminsimplifier/useradminsimplifier.php">'.__('Settings').'</a>';
+			$posk_links = '<a href="'.get_admin_url().'admin.php?page=useradminsimplifier/useradminsimplifier.php">'.__( 'Settings' ).'</a>';
 			// make the 'Settings' link appear first
 			array_unshift( $links, $posk_links );
 		}
@@ -60,11 +60,11 @@ License: GPLv2 or later
 	}
  	
 	function uas_add_admin_menu() {
-         add_management_page(	esc_html__( 'User Admin Simplifier', 'useradminsimplifier' ), 
-						esc_html__( 'User Admin Simplifier', 'useradminsimplifier' ), 
-						'manage_options', 
-						'useradminsimplifier/useradminsimplifier.php',
-						'useradminsimplifier_options_page' ); 
+         add_management_page( 	esc_html__( 'User Admin Simplifier', 'useradminsimplifier' ), 
+								esc_html__( 'User Admin Simplifier', 'useradminsimplifier' ), 
+								'manage_options', 
+								'useradminsimplifier/useradminsimplifier.php',
+								'useradminsimplifier_options_page' ); 
     }
 	
 	function uas_get_admin_options() {
@@ -76,8 +76,8 @@ License: GPLv2 or later
          update_option( 'useradminsimplifier_options', $uas_options );
     }
 	
-	function uas_clean_menu_name($menuname) { //clean up menu names provided by WordPress
- 		$menuname = preg_replace( '/<span (.*?)span>/' , '' , $menuname ); //strip the count appended to menus like the post count
+	function uas_clean_menu_name( $menuname ) { //clean up menu names provided by WordPress
+ 		$menuname = preg_replace( '/<span ( .*? )span>/' , '' , $menuname ); //strip the count appended to menus like the post count
 		return ( $menuname ); 
 	}	
 		
@@ -101,20 +101,19 @@ License: GPLv2 or later
   		if ( isset( $uas_options['selecteduser'] ) && $uas_options['selecteduser'] != $uas_selecteduser ) {
 			//user was changed
 			$uas_options['selecteduser'] = $uas_selecteduser;
-		} 
-		else {
+		} else {
 			$uas_options['selecteduser'] = $uas_selecteduser;
 			// process submitted menu selections
-			if ( isset ($_POST['uas_reset'] ) ) {
+			if ( isset ( $_POST['uas_reset'] ) ) {
 				//reset options for this user by clearing all their options 
 				unset ( $uas_options[ $uas_selecteduser ] );
  			}
 			else
 			{
-				if (isset ( $_POST['menuselection'] ) && is_array($_POST['menuselection'])) {
+				if ( isset ( $_POST['menuselection'] ) && is_array( $_POST['menuselection'] ) ) {
 					$menusectionsubmitted=true;
-					foreach ($_POST['menuselection'] as $key => $value) {
-							$nowselected[$uas_selecteduser][$value]=1; //disable this menu for this user
+					foreach ( $_POST['menuselection'] as $key => $value ) {
+						$nowselected[ $uas_selecteduser ][ $value] = 1; //disable this menu for this user
 					}
 				}
 			}
@@ -123,112 +122,105 @@ License: GPLv2 or later
 ?>
 <div class="wrap">
     <h2> 
-		<?php esc_html_e( 'User Admin Simplifier', 'user_admin_simplifier'); ?>
+		<?php esc_html_e( 'User Admin Simplifier', 'user_admin_simplifier' ); ?>
     </h2>
-    
     <form action="" method="post" id="uas_options_form" class="uas_options_form">
       	<div class="uas_container" id="chooseauser">
         <h3>
-        	<?php esc_html_e( 'Choose a user', 'user_admin_simplifier'); ?>: 
+        	<?php esc_html_e( 'Choose a user', 'user_admin_simplifier' ); ?>: 
         </h3>
         <select id="uas_user_select" name="uas_user_select" >
         <option>
 <?php
-			$blogusers = get_users('orderby=nicename');
-			foreach ($blogusers as $user) {
-				echo ('<option value="'. $user->user_nicename .'" '); 
+			$blogusers = get_users( 'orderby=nicename' );
+			foreach ( $blogusers as $user ) {
+				echo ( '<option value="'. $user->user_nicename .'" ' ); 
 				echo ( ( $user->user_nicename==$uas_selecteduser ) ? 'selected' : '' );
-				echo ('>' . $user->user_nicename .  '</option>');
+				echo ( '>' . $user->user_nicename .  '</option>' );
 			}
 ?>
 		 </select>
           </div>
-         
 <?php
 	        if( isset( $_POST['uas_user_select'] ) ) {
  ?>        
     <div class="uas_container" id="choosemenus">
         <h3>
-            <?php esc_html_e( 'Select menus/submenus to disable for this user', 'user_admin_simplifier'); ?>: 
+            <?php esc_html_e( 'Select menus/submenus to disable for this user', 'user_admin_simplifier' ); ?>: 
       </h3> 
         <input class="uas_dummy" style="display:none;" type="checkbox" checked="checked" value="uas_dummy" id="menuselection[]" name="menuselection[]">
 <?php
 				//lets start with top level menus stored in global $menu
 				//will add submenu support if needed later
- 				$rowcount=0; 
-				foreach($storedmenu as $menuitem) {
-					$menuuseroption=0;
-					if ( !('wp-menu-separator' == $menuitem[4]) ) {
-						//reset							$uas_options[$uas_selecteduser][$menuitem[5]]=0;
+ 				$rowcount = 0; 
+				foreach( $storedmenu as $menuitem ) {
+					$menuuseroption = 0;
+					if ( !( 'wp-menu-separator' == $menuitem[4] ) ) {
+						//reset							$uas_options[ $uas_selecteduser ][ $menuitem[5] ] = 0;
 						if ( $menusectionsubmitted ) {
-							if ( isset( $nowselected[$uas_selecteduser][$menuitem[5]] ) ) { //any selected options for this user/menu
-								$menuuseroption=$uas_options[$uas_selecteduser][$menuitem[5]]= $nowselected[$uas_selecteduser][$menuitem[5]] ;
+							if ( isset( $nowselected[ $uas_selecteduser ][ $menuitem[5] ] ) ) { //any selected options for this user/menu
+								$menuuseroption=$uas_options[ $uas_selecteduser ][ $menuitem[5] ] = $nowselected[ $uas_selecteduser ][ $menuitem[5] ] ;
 							} 
 							else {
-								$menuuseroption=$uas_options[$uas_selecteduser][$menuitem[5]]=0;
+								$menuuseroption=$uas_options[ $uas_selecteduser ][ $menuitem[5] ] = 0;
 							}
 						}
-						
-						if ( isset( $uas_options[$uas_selecteduser][$menuitem[5]] ) ) { //any saved options for this user/menu
-							$menuuseroption = $uas_options[$uas_selecteduser][$menuitem[5]];
+						if ( isset( $uas_options[ $uas_selecteduser ][ $menuitem[5] ] ) ) { //any saved options for this user/menu
+							$menuuseroption = $uas_options[ $uas_selecteduser ][ $menuitem[5] ];
 						} else {
-							$menuuseroption=0;
-							$uas_options[$uas_selecteduser][$menuitem[5]]=0;
+							$menuuseroption = 0;
+							$uas_options[ $uas_selecteduser ][ $menuitem[5] ] = 0;
 						}
- 						
 						//echo ( implode ( " ~ ",$menuitem ) );
 						//don't allow current user to disable their own access to the plugin
-						echo 	'<p class='. (( 0 == $rowcount++ %2 ) ? '"menumain"' : '"menualternate"' ) . '>'.
+						echo 	'<p class='. ( ( 0 == $rowcount++ %2 ) ? '"menumain"' : '"menualternate"' ) . '>'.
 						'<input type="checkbox" name="menuselection[]" id="menuselection[]" '.
-						'value="'. $menuitem[5] .'" ' . ( 1==$menuuseroption ? 'checked="checked"' : '') .
+						'value="'. $menuitem[5] .'" ' . ( 1==$menuuseroption ? 'checked="checked"' : '' ) .
  						' /> ' . 
-						uas_clean_menu_name($menuitem[0]) . "</p>";
+						uas_clean_menu_name( $menuitem[0] ) . "</p>";
 						if ( !( strpos( $menuitem[0], 'pending-count' ) ) ) { //top level menu items with pending count span don't have submenus
 							$topmenu=$menuitem[2];
 							//display submenus
-							if( isset(	$storedsubmenu[$topmenu] ) ) {
-							echo ( '<div class="submenu unselected"><a href="javascript:;">'. esc_html__( 'Show submenus', 'user_admin_simplifier').'</a></div><div class="submenuinner">' );
- 							$subrowcount=0;
-								foreach ($storedsubmenu[$topmenu] as $subsub) {
-									$combinedname=$menuitem[5].$subsub[2];
+							if( isset( 	$storedsubmenu[ $topmenu] ) ) {
+							echo ( '<div class="submenu unselected"><a href="javascript:;">'. esc_html__( 'Show submenus', 'user_admin_simplifier' ).'</a></div><div class="submenuinner">' );
+ 							$subrowcount = 0;
+								foreach ( $storedsubmenu[ $topmenu] as $subsub ) {
+									$combinedname = $menuitem[5].$subsub[2];
  									$submenuuseroption = 0;
-
-									//echo implode( ' - ',$subsub).        ' -  <br />';
+									//echo implode( ' - ',$subsub ).        ' -  <br />';
 									if ( $menusectionsubmitted ) {
-										if ( isset( $nowselected[$uas_selecteduser][$combinedname] ) ) { //any selected options for this user/submenu
-											$submenuuseroption=$uas_options[$uas_selecteduser][$combinedname]= $nowselected[$uas_selecteduser][$combinedname] ;
+										if ( isset( $nowselected[ $uas_selecteduser ][ $combinedname] ) ) { //any selected options for this user/submenu
+											$submenuuseroption=$uas_options[ $uas_selecteduser ][ $combinedname] = $nowselected[ $uas_selecteduser ][ $combinedname] ;
 										} 
 										else {
-											$submenuuseroption=$uas_options[$uas_selecteduser][$combinedname]=0;
+											$submenuuseroption=$uas_options[ $uas_selecteduser ][ $combinedname] = 0;
 										}
 									}
-									
-									if ( isset( $uas_options[$uas_selecteduser][$combinedname] ) ) { //any saved options for this user/submenu
-										$submenuuseroption = $uas_options[$uas_selecteduser][$combinedname];
+									if ( isset( $uas_options[ $uas_selecteduser ][ $combinedname] ) ) { //any saved options for this user/submenu
+										$submenuuseroption = $uas_options[ $uas_selecteduser ][ $combinedname];
 									} else {
-										$submenuuseroption=0;
-										$uas_options[$uas_selecteduser][$combinedname]=0;
+										$submenuuseroption = 0;
+										$uas_options[ $uas_selecteduser ][ $combinedname] = 0;
 									}
-										echo('<p class='. (( 0 == $subrowcount++ %2 ) ? '"submain"' : '"subalternate"' ) . '>'.
-											'<input type="checkbox" name="menuselection[]" id="menuselection[]" '.
-											'value="'. $combinedname .'" ' . ( 1==$submenuuseroption ? 'checked="checked"' : '') .
-											' /> '.uas_clean_menu_name($subsub[0]).'</p>');
+									echo( '<p class='. ( ( 0 == $subrowcount++ %2 ) ? '"submain"' : '"subalternate"' ) . '>'.
+										'<input type="checkbox" name="menuselection[]" id="menuselection[]" '.
+										'value="'. $combinedname .'" ' . ( 1==$submenuuseroption ? 'checked="checked"' : '' ) .
+										' /> '.uas_clean_menu_name( $subsub[0] ).'</p>' );
  								}
-								echo ('</div>');
+								echo ( '</div>' );
 							}
 						}
 					}
 				}
 ?>
-	<input name="uas_save" type="submit" id="uas_save" value="<?php esc_html_e( 'Save Changes', 'user_admin_simplifier'); ?>" /> <br />
-             <?php esc_html_e( 'or', 'user_admin_simplifier'); ?>: 
- 	<input name="uas_reset" type="submit" id="uas_reset" value="<?php esc_html_e( 'Clear User Settings', 'user_admin_simplifier'); ?>" />
+	<input name="uas_save" type="submit" id="uas_save" value="<?php esc_html_e( 'Save Changes', 'user_admin_simplifier' ); ?>" /> <br />
+             <?php esc_html_e( 'or', 'user_admin_simplifier' ); ?>: 
+ 	<input name="uas_reset" type="submit" id="uas_reset" value="<?php esc_html_e( 'Clear User Settings', 'user_admin_simplifier' ); ?>" />
 
     </div>
  <?php
 			}
 ?>
-
    </form>
 </div>
  <?php
@@ -238,29 +230,28 @@ uas_save_admin_options( $uas_options );
 	 function uas_admin_js() {
 ?>
 <script type="text/javascript">
-	jQuery(function() {
-		jQuery('form#uas_options_form #uas_user_select').change( function() {
-				jQuery('form#uas_options_form').submit();
-			}) 
-	});
-	
-	jQuery(document).ready(function () {
-		jQuery('div.submenuinner').slideUp('fast').hide();
+	jQuery( function() {
+		jQuery( 'form#uas_options_form #uas_user_select' ).change( function() {
+				jQuery( 'form#uas_options_form' ).submit();
+			} ) 
+	} );
+	jQuery( document ).ready( function () {
+		jQuery( 'div.submenuinner' ).slideUp( 'fast' ).hide();
 		//TO-DO: makes these submenu openings persist, save state in cookies?
-		jQuery('.submenu').click(function() {
-			inner=jQuery(this).next('.submenuinner');
-			if (jQuery(inner).is(":hidden")) {
-        		jQuery(inner).show().slideDown('fast');
-				jQuery(this).removeClass('unselected').addClass('selected');
-				jQuery(this).children('a').text('<?php esc_html_e( 'Hide submenus', 'user_admin_simplifier')?>');
+		jQuery( '.submenu' ).click( function() {
+			inner=jQuery( this ).next( '.submenuinner' );
+			if ( jQuery( inner ).is( ":hidden" ) ) {
+        		jQuery( inner ).show().slideDown( 'fast' );
+				jQuery( this ).removeClass( 'unselected' ).addClass( 'selected' );
+				jQuery( this ).children( 'a' ).text( '<?php esc_html_e( 'Hide submenus', 'user_admin_simplifier' )?>' );
 			} else {
-        		jQuery(inner).slideUp('fast').hide();
-				jQuery(this).removeClass('selected').addClass('unselected');
-				jQuery(this).children('a').text('<?php esc_html_e( 'Show submenus', 'user_admin_simplifier')?>');
+        		jQuery( inner ).slideUp( 'fast' ).hide();
+				jQuery( this ).removeClass( 'selected' ).addClass( 'unselected' );
+				jQuery( this ).children( 'a' ).text( '<?php esc_html_e( 'Show submenus', 'user_admin_simplifier' )?>' );
 
 			}
-    });
-});
+    } );
+} );
 </script>
 <?php
     }
@@ -268,12 +259,11 @@ uas_save_admin_options( $uas_options );
 ?>
 <style type="text/css">
 	 .unselected {
- 		background-image:url(<?php echo ( plugins_url( 'images/plus15.png' , __FILE__ ) ); ?>);
+ 		background-image:url( <?php echo ( plugins_url( 'images/plus15.png' , __FILE__ ) ); ?> );
 	}
 	 .selected {
-		background-image:url(<?php echo ( plugins_url( 'images/minus15.png' , __FILE__ ) ); ?>);
+		background-image:url( <?php echo ( plugins_url( 'images/minus15.png' , __FILE__ ) ); ?> );
  	}
-	
 	.submenu {
 		margin-left:200px;
 		padding-left:20px;
@@ -293,7 +283,6 @@ uas_save_admin_options( $uas_options );
 	.uas_options_form {
 		font-size:14px;
 	}
-	
 	.uas_options_form p {
 		margin:0 0;
 		padding:.5em .5em;
@@ -315,7 +304,6 @@ uas_save_admin_options( $uas_options );
 		padding:10px;
 		border-style:solid;
 	}
-
 	.submain {
 		background-color: #F3F3F3;
 	}
@@ -328,7 +316,6 @@ uas_save_admin_options( $uas_options );
 	.menualternate {
 		background-color: #DDDDDD;
 	}
-
 </style>
 <?php
     }
